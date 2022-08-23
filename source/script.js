f51b1b5930558deda1b0993db05d3d377c6c087a
daria konik <davkadaf@gmail.com>
2022-08-23 16:14:30 +0200

    main-nav corrected

--- source/script.js
+++ source/script.js
@@ -7,9 +7,13 @@ navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('main-nav--closed')) {
     navMain.classList.remove('main-nav--closed');
     navMain.classList.add('main-nav--opened');
+    navToggle.classList.remove('main-nav__toggle--closed');
+    navToggle.classList.add('main-nav__toggle--opened');
   } else {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
+    navToggle.classList.add('main-nav__toggle--closed');
+    navToggle.classList.remove('main-nav__toggle--opened');
   }
 });
 
