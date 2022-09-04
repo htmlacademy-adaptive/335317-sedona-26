06685c0fbf50569b9fe7088bff47c67abd57d8a5
daria konik <davkadaf@gmail.com>
2022-09-04 09:54:52 +0200

    menu js changes main-nav-list

--- source/script.js
+++ source/script.js
@@ -1,5 +1,6 @@
 let navMain = document.querySelector('.main-nav');
 let navToggle = document.querySelector('.main-nav__toggle');
+let navList = document.querySelector ('.main-nav__list');
 
 navMain.classList.remove('main-nav--nojs');
 
@@ -9,11 +10,13 @@ navToggle.addEventListener('click', function () {
     navMain.classList.add('main-nav--opened');
     navToggle.classList.remove('main-nav__toggle--closed');
     navToggle.classList.add('main-nav__toggle--opened');
+    navList.classList.remove('main-nav__list--closed');
   } else {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
     navToggle.classList.add('main-nav__toggle--closed');
     navToggle.classList.remove('main-nav__toggle--opened');
+    navList.classList.add('main-nav__list--closed');
   }
 });
 
